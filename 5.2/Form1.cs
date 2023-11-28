6e7c6096c80dc7544ff0989a0d9cc3873fce0c42
Huy Ngô <nh920211@gmail.com>
2023-11-28 09:08:11 +0700

    lab5_2

---
+++ 5.2/Form1.cs
@@ -0,0 +1,28 @@
+namespace _5._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void Button_thuchien(object sender, EventArgs e)
+        {
+            if(!double.TryParse(textBox1.Text, out double a))
+            {
+                MessageBox.Show("he thong khong hop le ");
+                return;
+            }
+        }
+    }
+}
